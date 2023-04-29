fe26f30c53683e31c6f976b2dc1d6a21fce2c994
Zemorath <trentward10@gmail.com>
2023-04-28 19:51:51 -0500

    Arithmetic Lab Complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 36;
+var num4 = 9;
+
+let mod = (num3/num4);
+
+let max = Math.max(1, 5, 10, 15, 20);
